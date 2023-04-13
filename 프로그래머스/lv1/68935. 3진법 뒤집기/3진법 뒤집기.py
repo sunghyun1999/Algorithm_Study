658b5eb5a1653d8b13fa0e07dd18ecf9a197c90d
Sunghyun Jung <32184099@dankook.ac.kr>
2023-04-13 20:00:57 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -0,0 +1,10 @@
+def radixChange(num, radix):
+    if num == 0: return 0
+    nums = []
+    while num:
+        num, digit = divmod(num, radix)
+        nums.append(str(digit))
+    return ''.join(reversed(nums))
+
+def solution(n):
+    return int(radixChange(n, 3)[::-1], 3)

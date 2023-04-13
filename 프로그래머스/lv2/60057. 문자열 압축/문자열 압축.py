2e484767250555edefe59c6e83efaad93c0b2766
Sunghyun Jung <32184099@dankook.ac.kr>
2023-04-13 17:45:20 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -0,0 +1,17 @@
+def solution(s):
+    answer = len(s)
+    for step in range(1, len(s)//2 + 1):
+        comp = ""
+        prev = s[0:step]
+        cnt = 1
+        for i in range(step, len(s), step):
+            if prev == s[i:i+step]:
+                cnt += 1
+            else:
+                comp += str(cnt) + prev if cnt >= 2 else prev
+                prev = s[i:i+step]
+                cnt = 1
+        comp += str(cnt) + prev if cnt >= 2 else prev
+        answer = min(answer, len(comp))
+    
+    return answer

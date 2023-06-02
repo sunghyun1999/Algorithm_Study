080041a5b94a112c81ad1f39324e14b14477b1c2
Sunghyun Jung <32184099@dankook.ac.kr>
2023-06-02 23:48:00 +0900

    [level 3] Title: 불량 사용자, Time: 4.30 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.py
@@ -0,0 +1,12 @@
+from itertools import permutations
+import re
+
+def solution(user_id, banned_id):
+    answer = set()
+    banned = ' '.join(banned_id).replace('*', '.')
+    
+    for i in permutations(user_id, len(banned_id)):
+        if re.fullmatch(banned, ' '.join(i)):
+            answer.add(''.join(sorted(i)))
+    
+    return len(answer)

d4777b538b6f439c3dfa69d6b036fd99dddd6a2a
Sunghyun Jung <32184099@dankook.ac.kr>
2023-07-10 23:04:52 +0900

    [Silver III] Title: N과 M (3), Time: 1384 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,13 @@
+def dfs():
+  if len(arr) == m:
+    print(' '.join(map(str, arr)))
+    return
+  for i in range(1, n+1):
+    arr.append(i)
+    dfs()
+    arr.pop()
+
+n, m = map(int, input().split())
+arr = []
+
+dfs()

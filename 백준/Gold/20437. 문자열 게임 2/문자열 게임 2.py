fc45a053f3ef691e17dbcfd438d280c5e087b1ac
Sunghyun Jung <32184099@dankook.ac.kr>
2023-10-07 11:34:06 +0900

    [Gold V] Title: 문자열 게임 2, Time: 2932 ms, Memory: 34160 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.py
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.py
@@ -1,23 +1,25 @@
 import sys
 input = sys.stdin.readline
+from collections import defaultdict
 
 t = int(input())
 for _ in range(t):
   max_value = 0
-  min_value = 10001
+  min_value = float('inf')
+
   w = input().strip()
   k = int(input())
+  count = defaultdict(list)
   for i in range(len(w)):
-    if w[i:].count(w[i]) >= k:
-      arr = []
-      for j in range(i, len(w)):
-        if w[j] == w[i]:
-          arr.append(j)
-          if len(arr) == k:
-            break
-      max_value = max(max_value, arr[-1]-arr[0]+1)
-      min_value = min(min_value, arr[-1]-arr[0]+1)
-  if max_value == 0 and min_value == 10001:
+    if w.count(w[i]) >= k:
+      count[w[i]].append(i)
+  for arr in count.values():
+    for j in range(len(arr)-k+1):
+      tmp = arr[j+k-1] - arr[j] + 1
+      min_value = min(min_value, tmp)
+      max_value = max(max_value, tmp)
+
+  if min_value == float('inf') and max_value == 0:
     print(-1)
   else:
     print(min_value, max_value)

2906a2dfb70af81efeb36679f1e6fd6a7ca4fb81
Sunghyun Jung <32184099@dankook.ac.kr>
2025-06-29 15:59:00 +0900

    [Silver I] Title: 단지번호붙이기, Time: 60 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,34 +1,39 @@
-import sys
-sys.setrecursionlimit(10**6)
+from collections import deque
 
-def dfs(x, y):
-  global cnt
-  if x < 0 or x >= n or y < 0 or y >= n:
-    return False
-
-  if graph[x][y] == 1:
-    graph[x][y] = 2
-    cnt += 1
-    dfs(x-1, y)
-    dfs(x+1, y)
-    dfs(x, y-1)
-    dfs(x, y+1)
-    return True
-  return False
-
-graph = []
 n = int(input())
+graph = []
 for i in range(n):
-  graph.append(list(map(int, input())))
+    graph.append(list(map(int, input())))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    graph[x][y] = 0
+    count = 1
+    
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                queue.append((nx, ny))
+                count += 1
+
+    return count
 
 result = []
-cnt = 0
-for i in range(n):
-  for j in range(n):
-    if dfs(i, j) == True:
-      result.append(cnt)
-      cnt = 0
 
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1:
+            result.append(bfs(i, j))
+
+result.sort()
 print(len(result))
-for i in sorted(result):
-  print(i)
+for r in result:
+    print(r)

74bb1dbf781e008bbcb9f3e6ce101da862e3f8d2
Sunghyun Jung <32184099@dankook.ac.kr>
2025-12-10 16:10:09 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.05 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -1,30 +1,29 @@
+def to_sec(time_str):
+    mm, ss = map(int, time_str.split(":"))
+    return mm * 60 + ss
+
+def to_mmss(sec):
+    mm = sec // 60
+    ss = sec % 60
+    return f"{mm:02d}:{ss:02d}"
+
 def solution(video_len, pos, op_start, op_end, commands):
-    video_min, video_sec = map(int, video_len.split(':'))
-    pos_min, pos_sec = map(int, pos.split(':'))
-    op_start_min, op_start_sec = map(int, op_start.split(':'))
-    op_end_min, op_end_sec = map(int, op_end.split(':'))
-    
-    video_total_sec = video_min * 60 + video_sec
-    pos_total_sec = pos_min * 60 + pos_sec
-    op_start_total_sec = op_start_min * 60 + op_start_sec
-    op_end_total_sec = op_end_min * 60 + op_end_sec
-    
-    curr = op_end_total_sec if op_start_total_sec <= pos_total_sec <= op_end_total_sec else pos_total_sec
+    video_len = to_sec(video_len)
+    pos = to_sec(pos)
+    op_start = to_sec(op_start)
+    op_end = to_sec(op_end)
+
+    if op_start <= pos <= op_end:
+        pos = op_end
+
     for cmd in commands:
-        if cmd == 'prev':
-            if curr < 10:
-                curr = 0
-            else:
-                curr -= 10
-        elif cmd == 'next':
-            if video_total_sec - curr < 10:
-                curr = video_total_sec
-            else:
-                curr += 10
-        
-        if op_start_total_sec <= curr <= op_end_total_sec:
-            curr = op_end_total_sec
-            
-    ans_min, ans_sec = curr // 60, curr % 60
+        if cmd == "prev":
+            pos = max(0, pos - 10)
+        elif cmd == "next":
+            pos = min(video_len, pos + 10)
+
+        if op_start <= pos <= op_end:
+            pos = op_end
+
+    return to_mmss(pos)
     
-    return f"{ans_min:02d}" + ':' + f"{ans_sec:02d}"
